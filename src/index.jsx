e7abf2b37f7a55f6cf8b3655b5a425544bb3fa2d
Mateusz Lewicki <mateusz.lewicki.ctr@sabre.com>
2024-03-26 11:54:37 +0100

    CSSIR-8817- enable showing imported requests

--- src/index.jsx
+++ src/index.jsx
@@ -10,26 +10,24 @@ const initialRequests = [];
 window.panelCreated = function appInit(devtools) {
   if (!root) {
     root = createRoot(document.getElementById('app'));
-    devtools.network.getHAR(async har => {
-      const graphQLEntries = har.entries.filter(entry => HarUtils.isGraphQLQuery(entry));
-      for (let i = 0; i < graphQLEntries.length; i++) {
-        const request = graphQLEntries[i];
-        const response = await new Promise(resolve => {
-          const onHarContentLoaded = content => {
-            resolve(JSON.parse(content));
-          };
-          request.getContent(onHarContentLoaded);
-        });
-        initialRequests.push(HarUtils.convertToTableModel(request, response));
-        const downlineRequests = response?.extensions?.downlineCommunication
-          ? response?.extensions?.downlineCommunication.map(downlineCommunication => HarUtils.fromDownlineCommunication(downlineCommunication))
-          : [];
-        delete response?.extensions?.downlineCommunication;
-        initialRequests.push(...downlineRequests);
-      }
-      root.render(<App onRequestFinished={devtools.network.onRequestFinished} initialRequests={initialRequests} />);
-    });
-  } else {
-    root.render(<App onRequestFinished={devtools.network.onRequestFinished} initialRequests={initialRequests} />);
   }
+  devtools.network.getHAR(async har => {
+    const graphQLEntries = har.entries.filter(entry => HarUtils.isGraphQLQuery(entry));
+    for (let i = 0; i < graphQLEntries.length; i++) {
+      const request = graphQLEntries[i];
+      const response = await new Promise(resolve => {
+        const onHarContentLoaded = content => {
+          resolve(JSON.parse(content));
+        };
+        request.getContent(onHarContentLoaded);
+      });
+      initialRequests.push(HarUtils.convertToTableModel(request, response));
+      const downlineRequests = response?.extensions?.downlineCommunication
+        ? response?.extensions?.downlineCommunication.map(downlineCommunication => HarUtils.fromDownlineCommunication(downlineCommunication))
+        : [];
+      delete response?.extensions?.downlineCommunication;
+      initialRequests.push(...downlineRequests);
+    }
+    root.render(<App onRequestFinished={devtools.network.onRequestFinished} initialRequests={initialRequests} />);
+  });
 };
